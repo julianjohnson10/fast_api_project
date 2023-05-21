d6d79b0ecc54b22a0d145ff88960a07c344425ce
julian.johnson.10 <julian.johnson.10@outlook.com>
2023-05-21 04:14:53 -0700

    updated views

--- backend/app/views.py
+++ backend/app/views.py
@@ -1,47 +1,48 @@
 from fastapi import APIRouter, Query
-from app.database import player_helper
+from app.database import player_helper, team_helper, MONGO_DETAILS, client, players_table, teams_table
 from motor import motor_asyncio
 from motor.motor_asyncio import AsyncIOMotorCursor
 
 router = APIRouter()
-MONGO_DETAILS = "mongodb://localhost:27017"
-client = motor_asyncio.AsyncIOMotorClient(MONGO_DETAILS)
-db = client['players_db']
-collection = db['players_collection']
 
 @router.get('/players')
 async def get_players(page: int = Query(1, ge=1), batch_size: int = Query(100, ge=1)):
     skip = (page - 1) * batch_size
     limit = batch_size
 
-    cursor: AsyncIOMotorCursor = collection.find().skip(skip).limit(limit)
+    cursor: AsyncIOMotorCursor = players_table.find().skip(skip).limit(limit)
     players = await cursor.to_list(None)
 
     return [player_helper(player) for player in players]
 
 @router.get('/all_players')
 async def get_players():
-    cursor: AsyncIOMotorCursor = collection.find()
+    cursor: AsyncIOMotorCursor = players_table.find()
     players = await cursor.to_list(None)
 
     return [player_helper(player) for player in players]
 
-# @router.get('/players/{searched_player_paginated}')
-# async def get_players_searched(searched_player: str, page: int = Query(1, ge=1), batch_size: int = Query(100, ge=1)):
-#     skip = (page - 1) * batch_size
-#     limit = batch_size
-#     substring = searched_player
-#     pattern = f".*{substring}.*"
-#     query = {"full_name": {"$regex": pattern, "$options": "i"}}
-#     cursor: AsyncIOMotorCursor = collection.find(query).skip(skip).limit(limit)
-#     players = await cursor.to_list(None)
-#     return [player_helper(player) for player in players]
+@router.get('/all_teams')
+async def get_teams():
+    cursor: AsyncIOMotorCursor = teams_table.find()
+    teams = await cursor.to_list(None)
+
+    return [team_helper(team) for team in teams]
 
 @router.get('/players/{searched_player}')
 async def get_players_searched(searched_player: str):
     substring = searched_player
     pattern = f".*{substring}.*"
     query = {"full_name": {"$regex": pattern, "$options": "i"}}
-    cursor: AsyncIOMotorCursor = collection.find(query)
+    cursor: AsyncIOMotorCursor = players_table.find(query)
     players = await cursor.to_list(None)
     return [player_helper(player) for player in players]
+
+@router.get('/teams/{searched_team}')
+async def get_teams_searched(searched_team: str):
+    substring = searched_team
+    pattern = f".*{substring}.*"
+    query = {"full_name": {"$regex": pattern, "$options": "i"}}
+    cursor: AsyncIOMotorCursor = teams_table.find(query)
+    teams = await cursor.to_list(None)
+    return [team_helper(team) for team in teams]
